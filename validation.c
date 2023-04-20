7fbe461f0dc274ad2698527c204914bc1022bee8
rodrigo <rodrigoadcarvalho@gmail.com>
2023-04-20 09:24:13 -0300

    working on validation

--- validation.c
+++ validation.c
@@ -1,5 +1,25 @@
 #include "push_swap.h"
 
+//argument is empty
+int check_empty(int argc, char **argv)
+{
+    int i;
+
+    i = 1;
+    if (!(argv[i]))
+        return (1);
+    while (i < argc)
+    {
+    
+        if (ft_strlen(argv[i]) == 0)
+        {
+            return (1);
+        }
+        i++;
+    }
+    return (0);
+}
+
 //argument is digit (has char?)
 int check_char(int argc, char **argv)
 {
@@ -51,8 +71,8 @@ int check_dup(int argc, char **argv)
     i = 1;
     while (i < argc)
     {
-        j = 0;
-        while (argv[j])
+        j = i + 1;
+        while (j < argc)
         {
             if (ft_atol(argv[i]) == ft_atol(argv[j]))
             {
@@ -68,15 +88,25 @@ int check_dup(int argc, char **argv)
 //all validations
 int validation(int argc, char **argv)
 {
-    if(check_char(argc, argv) == 0)
+    if (check_empty(argc, argv) == 1)
     {
-        if (check_max_min_int(argc, argv) == 0)
-        {
-            if (check_dup(argc, argv) == 0)
-            {
-                return (0);
-            }
-        }
+        ft_printf("argument empty\n");
+        return (-1);
     }
-    return (1);
+    if(check_char(argc, argv) == 1)
+    {
+        ft_printf("argument has char\n");
+        return (-2);
+    }
+    if (check_max_min_int(argc, argv) == 1)
+    {
+        ft_printf("argument has max or min int\n");
+        return (-3);
+    }
+    if (check_dup(argc, argv) == 1)
+    {
+        ft_printf("argument has duplicate\n");
+        return (-4);
+    }
+    return (0);
 }
