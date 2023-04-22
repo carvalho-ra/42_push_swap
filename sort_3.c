70bb2888c7d886b2e974801c9ea0f3b667754af9
rodrigo <rodrigoadcarvalho@gmail.com>
2023-04-22 16:17:34 -0300

    included case 3 in sort_3 - number of operations - OK- valgrind --leak-check=yes - OK

--- sort_3.c
+++ sort_3.c
@@ -5,7 +5,10 @@ int sort_3(t_list *list)
 {
     while (!(is_sorted(list)))
     {
-        if ((list->head->data > list->head->next->data))
+        if ((list->head->data > list->tail->data) && 
+            (list->tail->data > list->head->next->data))
+            rotate(list);
+        else if (list->head->data > list->head->next->data)
             swap(list);
         else
             rev_rotate(list);
